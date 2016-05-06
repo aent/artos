6b8e4a1bb7cfda98b44454f29db5cfd23f93c29a
Alexander Entinger <cto@lxrobotics>
2016-05-06 12:20:04 +0200

    Adding I2CInterface class, DriverInterface class and a subfolder with a first draft of the DRV2605 driver

---
+++ drivers/DRV2605/DRV2605.cpp
@@ -0,0 +1,80 @@
+/**
+ * Copyright (c) 2016, Alexander Entinger
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of artos nor the names of its
+ *  contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**************************************************************************************
+ * INCLUDES
+ **************************************************************************************/
+
+#include "DRV2605.h"
+
+namespace artos
+{
+
+/**************************************************************************************
+ * PUBLIC METHODS
+ **************************************************************************************/
+
+DRV2605::DRV2605(I2CInterface *i2c, uint8_t const i2c_address)
+{
+  this->i2c = i2c;
+  this->i2c_address = i2c_address;
+}
+
+DRV2605::~DRV2605()
+{
+
+}
+
+void DRV2605::open()
+{
+
+}
+
+void DRV2605::close()
+{
+
+}
+
+void DRV2605::read(uint8_t *buffer, uint8_t const bytes)
+{
+
+}
+
+void DRV2605::write(uint8_t const buffer, uint8_t const bytes)
+{
+
+}
+
+void DRV2605::ioctl(void *arg)
+{
+
+}
+
+} // namespace artos
